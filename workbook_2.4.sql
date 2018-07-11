7d84f0601805daeb70b4529fab955acedf391a55
John Oberholtzer <johnpauloberholtzer@gmail.com>
2018-07-11 10:36:43 -0500

    Question 2.4

---
+++ workbook_2.4.sql
@@ -0,0 +1,12 @@
+conn chinook/p4ssw0rd
+
+UPDATE CUSTOMER 
+SET LASTNAME = 'Mitchell' ,FIRSTNAME = 'Aaron' 
+WHERE LASTNAME = 'Walter' AND FIRSTNAME = 'Robert';
+
+UPDATE ARTIST
+SET NAME = 'CCR'
+WHERE NAME = 'Creedence Clearwater Revival';
+
+exit;
+
---
+++ workbook_2.4_output.sql
@@ -0,0 +1,9 @@
+Connected.
+
+0 rows updated.
+
+
+1 row updated.
+
+
+Disconnected from Oracle Database 11g Express Edition Release 11.2.0.2.0 - 64bit Production
