12b720ba48e175e4ddeb3adf158fc58ae141b61c
John Oberholtzer <johnpauloberholtzer@gmail.com>
2018-07-11 10:02:04 -0500

    Updated Question 2.1

--- workbook_2.1.sql
+++ workbook_2.1.sql
@@ -3,7 +3,7 @@ conn chinook/p4ssw0rd
 SHOW TABLES;
 SELECT * FROM Employee;
 SELECT * FROM Employee where LASTNAME = 'King';
-SELECT * FROM Employee where FIRSTNAME = 'Andrew';
+SELECT * FROM Employee where FIRSTNAME = 'Andrew' AND REPORTSTO = NULL;
 
 exit;
 
