114e192728761295ae9d6ee2e0611b65593a67da
John Oberholtzer <johnpauloberholtzer@gmail.com>
2018-07-11 10:26:52 -0500

    Question 2.3

---
+++ workbook_2.3_output.sql
@@ -0,0 +1,21 @@
+Connected.
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+Disconnected from Oracle Database 11g Express Edition Release 11.2.0.2.0 - 64bit Production
