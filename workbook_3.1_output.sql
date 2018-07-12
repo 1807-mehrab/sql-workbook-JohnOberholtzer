b32187e2013e8390e0f8a7bfbdc7ec4d09ff0a8b
John Oberholtzer <johnpauloberholtzer@gmail.com>
2018-07-12 15:14:12 -0500

    Question 3.1

---
+++ workbook_3.1_output.sql
@@ -0,0 +1,9 @@
+Connected.
+
+Function GET_TIME compiled
+
+
+PL/SQL procedure successfully completed.
+
+
+Disconnected from Oracle Database 11g Express Edition Release 11.2.0.2.0 - 64bit Production
