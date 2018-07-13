3b1b93e5e0df788f9275e95f06a29d696b584d48
John Oberholtzer <johnpauloberholtzer@gmail.com>
2018-07-13 12:58:10 -0500

    Fixed 3.1

--- workbook_3.1_output.sql
+++ workbook_3.1_output.sql
@@ -1,4 +1,3 @@
-Connected.
 
 Function GET_TIME compiled
 
@@ -6,6 +5,19 @@ Function GET_TIME compiled
 Function MEDIA_LENGTH compiled
 
 
+GET_TIME                       
+-------------------------------
+13-JUL-18 12.55.17.332000000 PM
+
+
+MEDIA_LENGTH(1)
+---------------
+             15
+             15
+             15
+             15
+             15
+
+
 PL/SQL procedure successfully completed.
 
-Connection created by CONNECT script command disconnected
---
+++ workbook_3.1_output_Dbms.sql
@@ -0,0 +1,3 @@
+13-JUL-18 12.55.17.344000 PM
+Length of first Mediatype: 15
+
